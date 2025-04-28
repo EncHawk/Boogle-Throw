2a903389583013186762f83d191d1d647799675d
Dilip Kumar R <dilipkumar2000.r@gmail.com>
2025-04-28 14:36:40 +0530

    its somehwat working. need to fix ui, fuck im shaking!

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ document.getElementById('header-input').addEventListener('input', saveHeader); /
 document.getElementById('body-input').addEventListener('input', saveBody);
 
 // loading all the existing data to the index page from the local storage.
-// loadData();
+loadData();
 
 
 function saveHeader(){ //logic for the header data to be stored
@@ -42,36 +42,34 @@ function saveBody() {
 
 //test text to add into the created element
 
-const testTxt= []; // its not a test, i dont have the patience to fix the name. 
+const userResponse= []; // its not a test, i dont have the patience to fix the name. 
 // TODO: fix this name
+const storedData = JSON.parse(localStorage.getItem('user-data-array')) || [];
+userResponse.push(...storedData); // ... allows the unpacking of elements inside as push expects only one element and not an array.
 
-let counterVar=0;
+// let counterVar=0;
 
 
 document.getElementById('create-btn').addEventListener('click', ()=>{ // clicking the create button makes js to create a div that will store the boxes
-  testTxt.push({
+  userResponse.push({
     title:Header,
     content: Body,
     date: '20/12/05'
   });
-
+  localStorage.setItem('user-data-array', JSON.stringify(userResponse));
     
   document.getElementById('body-input').value= '';
   document.getElementById('header-input').value= '';
-  
-  
-  createCard(testTxt);
-
-  counterVar++; // increments the counter to access the next element in the array.
-
-
-  JSON.stringify(localStorage.setItem('user-data-array', testTxt));
-  console.log(testTxt);
+  createCard(userResponse);
+  // counterVar++; // increments the counter to access the next element in the array.
+  // console.log(userResponse);
 });
 // error fix: the cardholder couldnt access the newBox since it was outside. added it inside therefore it can see the existense of newBox
 
 
-function createCard(testTxt){
+function createCard(userResponse){
+  let size = userResponse.length;
+  let object= userResponse[size-1];
   
   const newBox= document.createElement('div');  
   newBox.className="container-fluid card";
@@ -79,25 +77,24 @@ function createCard(testTxt){
 
   const iTitle = document.createElement('h3');
   iTitle.className="card-inner-title";
-  iTitle.innerHTML=testTxt[counterVar].title;
+  iTitle.innerHTML=object.title;
 
   const iContent = document.createElement('p');
   iContent.className="card-inner-content";
-  iContent.innerHTML=testTxt[counterVar].content;
+  iContent.innerHTML=object.content;
 
   const iDate = document.createElement('p');
   iDate.className="card-inner-date";
-  iDate.innerHTML=testTxt[counterVar].date;
+  iDate.innerHTML=object.date;
 
-  
+
 
   let cardHolder= document.getElementById('outer-wrapper');
   newBox.appendChild(iTitle);
   newBox.appendChild(iContent);
   newBox.appendChild(iDate);
   cardHolder.appendChild(newBox);
-
-
+  
 }
 
 function addData(dataArray){
@@ -118,7 +115,7 @@ function addData(dataArray){
     iDate.className="card-inner-date";
     iDate.innerHTML=object.date;
 
-    
+
 
     let cardHolder= document.getElementById('outer-wrapper');
     newBox.appendChild(iTitle);
@@ -129,13 +126,16 @@ function addData(dataArray){
 }
 
 function loadData(){
-  if (localStorage.getItem('user-data-array')){
-    const dataArray = JSON.parse(localStorage.getItem('user-data-array'));
-    console.log(dataArray +"from load data");
-    addData(dataArray);
-
+  const localData = JSON.parse(localStorage.getItem('user-data-array')) || []; // if it exists in LS then take it else become an empty array
+  if(localData!=[]){
+    console.log(localData);
+    addData(localData);
   }
   else{
-    document.getElementById('outer-wrapper').innerHTML = `No content to load, gimme somthing to store daddy! im hunrgy :O`;
+    alert('ntg to retrieve from the local storage, feed me daddy im hungry :)');
   }
 }
+
+// function clearFn(){
+//   localStorage.clear();
+// }
